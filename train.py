e0de8e4e8c87e047797805f4711a869e9c5d5625
Sandaruwan-Jayasundara <sandaruwan.jaya98@gmail.com>
2022-05-29 14:26:07 +0530

    check the accuracy in linear regression, decision tree and random forest algorithms

--- train.py
+++ train.py
@@ -28,3 +28,29 @@ y= vehicle_data['Price']
 
 # define train and test and test data size
 x_train_axis, X_test_axis, y_train_axis, y_test_axis = train_test_split(x, y, test_size=0.25)
+
+# print the shape of the axis
+print(x_train_axis.shape, X_test_axis.shape)
+
+# define a method to check the accuracy of the models
+def model_accuracy(model):
+    model.fit(x_train_axis,y_train_axis)
+    accuracy = model.score(X_test_axis,y_test_axis)
+    print(str(model)+'=>'+str(accuracy))
+
+# use algorithms to select the highest accuracy algorithm
+# Linear Regression Algorithm
+# Decision Tree Algorithm
+# Random Forest Algorithm
+
+# check linear regression accuracy
+LinearReg=LinearRegression()
+model_accuracy(LinearReg)
+
+# check decision tree accuracy
+DecisionTree=DecisionTreeRegressor()
+model_accuracy(DecisionTree)
+
+# check random forest accuracy
+RandomForest=RandomForestRegressor()
+model_accuracy(RandomForest)
