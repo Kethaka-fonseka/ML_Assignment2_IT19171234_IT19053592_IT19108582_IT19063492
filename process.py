295762fa6ced38245996eb5caffcb2b9cd220a8d
Kethaka-fonseka <kethaka.fsk@gmail.com>
2022-05-29 14:50:08 +0530

    define a method set_brand  to rename the brands

--- process.py
+++ process.py
@@ -2,3 +2,11 @@ import analize as ppcd
 
 # get the vehicle data from the analize file
 vehicle_data = ppcd.vehicle_data
+
+# define the set_brand method to rename the brands in the data set
+def set_brand(name):
+    if name == 'Honda' or name == 'Toyota' or name == 'BMW' or name == 'Suzuki' or name == 'Mitsubishi':
+        return name
+    else:
+        return 'Other'
+
