7aedc45e759ad0550b034eb1b93e84af97897763
Ushanjay <jayawardhanausan@gmail.com>
2022-05-29 16:57:52 +0530

    print vehicle data

--- collection.py
+++ collection.py
@@ -5,3 +5,9 @@ import matplotlib.pyplot as plt
 # import the data set
 vehicle_data = pd.read_csv('vehicle_data.csv')
 
+# We used the below code to reduce the number of records in the data file
+# vehicle_data.drop(vehicle_data.index[1001:4123970], inplace=True)
+
+# print the vehicle data sets
+print(vehicle_data.tail())
+
