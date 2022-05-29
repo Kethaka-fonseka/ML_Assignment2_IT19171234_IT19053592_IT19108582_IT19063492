7dbe5e779b563541df3ed4f8bf4366c60ce073ac
Sandaruwan-Jayasundara <sandaruwan.jaya98@gmail.com>
2022-05-29 14:14:59 +0530

    import all libraries

---
+++ train.py
@@ -0,0 +1,9 @@
+import process as prc
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+
