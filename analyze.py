3dff330098d35cc13c28d63e3a65a12a31670c6b
mandara-fernando <mandarajfx@gmail.com>
2022-05-29 15:39:12 +0530

    Removed the comma from price column

--- analyze.py
+++ analyze.py
@@ -3,3 +3,19 @@ import pandas as pd
 
 # get the vehicle data from the collectData file
 print(clt.vehicle_data.head())
+
+vehicle_data = clt.vehicle_data
+
+# Print the Vehicle data
+print(vehicle_data.head())
+
+# Remove currency name from the price column
+vehicle_data['Price'] = vehicle_data['Price'].str.replace(r"[a-zA-Z]", '0')
+
+# Remove the spaces from the price
+vehicle_data['Price'] = vehicle_data['Price'].fillna('').str.replace(' ', "")
+
+# Remove the comma from the Price column
+vehicle_data['Price'] = vehicle_data['Price'].fillna('').str.replace(',', "").astype(float)
+
+
